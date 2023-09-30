7296ae731857be2119e289f76aa2992927b70e2c
lavinas <lavinas@me.com>
2023-09-30 19:17:44 -0300

    Testing handler

---
+++ internal/client/adapter/hdlr/hdlr_gin_test.go
@@ -0,0 +1,32 @@
+package hdlr
+
+import (
+	"net/http/httptest"
+	"testing"
+	"net/http"
+
+)
+
+func TestCreate(t *testing.T) {
+	w := httptest.NewRecorder()
+	ctx := GetTestGinContext(w)
+	content := map[string]interface{}{
+		"name": "Jose da Silva",
+		"nickname": "jose_da_silva_222",
+		"document": "206.656.600-49",
+		"phone": "+55 (11) 99999-9999",
+		"email": "test@test.com.br",
+	}
+	MockJsonPost(ctx, content)
+	l := LogMock{}
+	s := ServiceMock{}
+
+	h := NewHandlerGin(&l, &s)
+	h.Create(ctx)
+
+	if w.Code != http.StatusCreated {
+		t.Errorf("Invalid result: %v", w.Code)
+	}	
+
+}
+
---
+++ internal/client/adapter/hdlr/hdlr_gin_test_mock.go
@@ -0,0 +1,83 @@
+package hdlr
+
+import (
+	"os"
+	"io"
+	"bytes"
+	"net/http"
+	"net/url"
+	"net/http/httptest"
+	"encoding/json"
+
+	"github.com/gin-gonic/gin"
+	"github.com/lavinas/keel/internal/client/core/port"
+)
+
+//mock gin context
+func GetTestGinContext(w *httptest.ResponseRecorder) *gin.Context {
+	gin.SetMode(gin.TestMode)
+
+	ctx, _ := gin.CreateTestContext(w)
+	ctx.Request = &http.Request{
+	Header: make(http.Header),
+	URL:    &url.URL{},
+	}
+
+	return ctx
+}
+
+//mock postrequest
+func MockJsonPost(c *gin.Context, content interface{}) {
+	c.Request.Method = "POST"
+	c.Request.Header.Set("Content-Type", "application/json")
+	c.Set("user_id", 1)
+
+	jsonbytes, err := json.Marshal(content)
+	if err != nil {
+		panic(err)
+	}
+
+	// the request body must be an io.ReadCloser
+	// the bytes buffer though doesn't implement io.Closer,
+	// so you wrap it in a no-op closer
+	c.Request.Body = io.NopCloser(bytes.NewBuffer(jsonbytes))
+}
+
+
+// Log Mock
+type LogMock struct {
+	mtype string
+	msg   string
+}
+func (l *LogMock) GetFile() *os.File {
+	return nil
+}
+func (l *LogMock) Info(msg string) {
+	l.mtype = "Info"
+	l.msg = msg
+}
+func (l *LogMock) Infof(input any, message string) {
+	l.mtype = "Info"
+	b, _ := json.Marshal(input)
+	l.Info(message + " | " + string(b))
+}
+func (l *LogMock) Error(msg string) {
+	l.mtype = "Error"
+	l.msg = msg
+}
+func (l *LogMock) Errorf(input any, err error) {
+	b, _ := json.Marshal(input)
+	l.Error(err.Error() + " | " + string(b))
+}
+func (l *LogMock) Close() {
+}
+
+
+// Service Mock
+type ServiceMock struct {
+}
+
+func (s *ServiceMock) Create(input port.CreateInputDto, output port.CreateOutputDto) error {
+	output.Fill("1", "name", "nickname", "document", "phone", "email")
+	return nil
+}
