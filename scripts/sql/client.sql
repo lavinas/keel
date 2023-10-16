73d52d37cc60ea48c6085c928a9e3e8c82da474c
lavinas <lavinas@me.com>
2023-10-16 20:14:39 -0300

    invoice.sql

--- scripts/sql/client.sql
+++ scripts/sql/client.sql
@@ -7,19 +7,10 @@ CREATE TABLE keel_client.client (
   document DECIMAL(20) NOT NULL,
   phone DECIMAL(20) NOT NULL,
   email VARCHAR(50) NOT NULL,
-  PRIMARY KEY (id)
+  INDEX idx_nick (nickname ASC),
+  INDEX idx_document (document ASC),
+  INDEX idx_email (email ASC),
+  INDEX idx_phone (phone ASC)
 );
-
-ALTER TABLE keel_client.client 
-  ADD INDEX idx_nick (nickname ASC);
-
-ALTER TABLE keel_client.client 
-  ADD INDEX idx2 (document ASC);
-
-ALTER TABLE keel_client.client 
-  ADD INDEX idx3 (email ASC);
-
-ALTER TABLE keel_client.client 
-  ADD INDEX idx4 (phone ASC);
 
   
--- scripts/sql/invoice.sql
+++ scripts/sql/invoice.sql
@@ -1,40 +1,47 @@
 # INVOICES
 
 CREATE TABLE keel_invoice.invoice_status(
-    id VARCHAR(50) NOT NULL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    PRIMARY KEY (id),
-)
+    id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
 
 CREATE TABLE keel_invoice.invoice(
     id VARCHAR(50) NOT NULL PRIMARY KEY,
-    sequence VARCHAR(10) NOT NULL,
-    client_reference VARCHAR(50) NOT NULL,
-    client_name VARCHAR(100) NOT NULL,
-    client_document DECIMAL(20) NOT NULL,
+    reference VARCHAR(10) NOT NULL,
+    client_id VARCHAR(50) NOT NULL,
     client_email VARCHAR(50) NOT NULL,
+    client_nickname VARCHAR(50) NULL,
+    client_name VARCHAR(100) NULL,
+    client_document DECIMAL(20) NULL,
+    client_phone DECIMAL(20) NULL,
     date DATE NOT NULL,
     due DATE NOT NULL,
     notes VARCHAR(200) NOT NULL,
-    status_id VARCHAR(10) NOT NULL,
+    status_id INT NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    PRIMARY KEY (id),
-    INDEX idx_number (number ASC),
-    INDEX idx_client (client_id ASC),
-    FOREIGN KEY (status_id) REFERENCES keel_invoice.invoice_status(id),
-)
+    INDEX idx_reference (reference ASC),
+    INDEX idx_client_id (client_id ASC),
+    INDEX idx_date (date ASC),
+    INDEX idx_due (due ASC),
+    INDEX idx_status (status_id ASC),
+    FOREIGN KEY (client_id) REFERENCES keel_client.client(id),
+    FOREIGN KEY (status_id) REFERENCES keel_invoice.invoice_status(id)
+);
 
 CREATE TABLE keel_invoice.invoice_item(
     id VARCHAR(50) NOT NULL PRIMARY KEY,
     invoice_id VARCHAR(50) NOT NULL,
-    service_reference VARCHAR(50) NOT NULL,
-    service_description VARCHAR(200) NOT NULL,
-    service_value DECIMAL(20, 2) NOT NULL,
-    service_quantity DECIMAL(10) NOT NULL,
+    service_id VARCHAR(50) NOT NULL,
+    service_name VARCHAR(50) NOT NULL,
+    description VARCHAR(200) NULL,
+    value DECIMAL(20, 2) NOT NULL,
+    quantity DECIMAL(10) NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (invoice_id) REFERENCES invoice(id),
+    INDEX idx_invoice_id (invoice_id ASC),
+    INDEX idx_service_id (service_id ASC),
+    FOREIGN KEY (invoice_id) REFERENCES invoice(id)
 );
+
 
