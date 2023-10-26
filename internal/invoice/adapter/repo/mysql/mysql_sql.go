38159483d407423f3a66b9b703c1ee05bac322e5
lavinas <lavinas@me.com>
2023-10-26 14:54:32 -0300

    testing invoice mysql repository

--- internal/invoice/adapter/repo/mysql/mysql_sql.go
+++ internal/invoice/adapter/repo/mysql/mysql_sql.go
@@ -6,8 +6,8 @@ var (
 		"SaveInvoiceClient":     "INSERT INTO {DB}.invoice_client (id, nickname, client_id, name, document, phone, email) VALUES (?, ?, ?, ?, ?, ?, ?);",
 		"SaveInvoice":           "INSERT INTO {DB}.invoice (id, reference, business_id, customer_id, amount, date, due, note_id, status_id, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);",
 		"SaveInvoiceItem":       "INSERT INTO {DB}.invoice_item (id, invoice_id, service_reference, description, amount, quantity) VALUES (?, ?, ?, ?, ?, ?);",
-		"TruncateInvoiceClient": "TRUNCATE TABLE {DB}.invoice_client;",
-		"TruncateInvoice":       "TRUNCATE TABLE {DB}.invoice;",
-		"TruncateInvoiceItem":   "TRUNCATE TABLE {DB}.invoice_item;",
+		"TruncateInvoiceClient": "DELETE FROM {DB}.invoice_client;",
+		"TruncateInvoice":       "DELETE FROM {DB}.invoice;",
+		"TruncateInvoiceItem":   "DELETE FROM {DB}.invoice_item;",
 	}
 )
