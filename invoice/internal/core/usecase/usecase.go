eeb2a31b3ba89cb8ce9819d40f22023f2350fdb4
lavinas <lavinas@me.com>
2023-11-19 18:31:20 -0300

    gofmt

--- invoice/internal/core/usecase/usecase.go
+++ invoice/internal/core/usecase/usecase.go
@@ -6,21 +6,21 @@ import (
 )
 
 // Service is the usecase handler for the application
-type Service struct {
+type UseCase struct {
 	repo   port.Repository
 	logger port.Logger
 	config port.Config
 }
 
-func NewService(repo port.Repository, logger port.Logger, config port.Config) *Service {
-	return &Service{
+func NewService(repo port.Repository, logger port.Logger, config port.Config) *UseCase {
+	return &UseCase{
 		repo:   repo,
 		logger: logger,
 		config: config,
 	}
 }
 
-func (s *Service) RegisterClient(dto *dto.RegisterInvoiceClient) error {
+func (s *UseCase) RegisterClient(dto *dto.RegisterInvoiceClient) error {
 	if err := dto.Validate(); err != nil {
 		return err
 	}
