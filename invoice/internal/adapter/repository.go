dfdf54b9783b312fb41f74db74b03b2f1454e9dc
lavinas <lavinas@me.com>
2023-11-19 18:46:06 -0300

    gofmt

--- invoice/internal/adapter/repository.go
+++ invoice/internal/adapter/repository.go
@@ -22,7 +22,7 @@ type Repository struct {
 
 // NewRepository creates a new repository handler
 func NewRepository(config port.Config) (*Repository, error) {
-	db, err := ConnectDB(config)
+	db, err := connectDB(config)
 	if err != nil {
 		return nil, err
 	}
@@ -40,28 +40,9 @@ func (r *Repository) Close() {
 }
 
 // ConnectDB connects to the database
-func ConnectDB(config port.Config) (*gorm.DB, error) {
-	host := config.Get(MYSQL_HOST)
-	if host == "" {
-		host = "localhost"
-	}
-	user := config.Get(MYSQL_USER)
-	if user == "" {
-		user = "root"
-	}
-	pass := config.Get(MYSQL_PASSWORD)
-	if pass == "" {
-		pass = "root"
-	}
-	dab := config.Get(MYSQL_DATABASE)
-	if dab == "" {
-		dab = "keel_invoice"
-	}
-	mode := config.Get(MYSQL_SSLMODE)
-	if mode == "" {
-		mode = "disable"
-	}
-	dns := fmt.Sprintf("dbname=%s sslmode=%s user=%s password=%s host=%s", dab, mode, user, pass, host)
+func connectDB(config port.Config) (*gorm.DB, error) {
+	dns := fmt.Sprintf("dbname=%s sslmode=%s user=%s password=%s host=%s", config.Get(MYSQL_DATABASE),
+		config.Get(MYSQL_SSLMODE), config.Get(MYSQL_USER), config.Get(MYSQL_PASSWORD), config.Get(MYSQL_HOST))
 	db, err := gorm.Open("postgres", dns)
 	if err != nil {
 		return nil, err
