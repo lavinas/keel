c75d08a1191d94a966c8dfe795c0672505d99ca9
lavinas <lavinas@me.com>
2023-11-24 13:22:36 -0300

    New usecase

--- invoice/internal/core/usecase/usecase.go
+++ invoice/internal/core/usecase/usecase.go
@@ -1,6 +1,8 @@
 package usecase
 
 import (
+	"net/http"
+
 	"github.com/lavinas/keel/invoice/internal/core/port"
 )
 
@@ -23,3 +25,25 @@ func NewUseCase(config port.Config, logger port.Logger, repo port.Repository) *U
 		config: config,
 	}
 }
+
+func (s *UseCase) Register(domain port.Domain, result port.DefaultResult) {
+	domain.SetBusinessID(s.config.Get(BUSINNESS_ID))
+	if err := domain.Validate(); err != nil {
+		s.logger.Info("xxxx client - create domain")
+		result.Set(http.StatusInternalServerError, "internal error")
+		return
+	}
+	if err := s.repo.Add(domain); err != nil {
+		if s.repo.IsDuplicatedError(err) {
+			s.logger.Info("xxxx client - add client")
+			result.Set(http.StatusConflict, "client id already exists")
+			return
+		}
+		s.logger.Info("xxxxx client - add client")
+		result.Set(http.StatusInternalServerError, "internal error")
+		return
+	}
+	s.logger.Info("xxxxx client - add client")
+	result.Set(http.StatusCreated, "client created")
+}
+
